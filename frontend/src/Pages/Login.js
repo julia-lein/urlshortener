d5711cdd5bac2b66903dbec11868bc1ab93ffd90
Julia Lein <julia.lein77@gmail.com>
2021-09-06 09:33:34 +0200

    Login page

--- frontend/src/Pages/Login.js
+++ frontend/src/Pages/Login.js
@@ -2,9 +2,25 @@ import React from 'react'
 
 function Login() {
     return (
-        <div>
-            login
-        </div>
+                
+             <div className="container col-xxl-8 px-4 py-5">
+                <div>
+                    <form className="form-floating row flex-lg-row g-5 py-5" action="">
+                        <div className="col-sm-6 mx-auto">
+                            <div className="form-floating mb-3">
+                                <input type="text" className="form-control" id="inputLink" required />
+                                <label for="inputText">Email:</label>
+                            </div>
+                            <div className="form-floating mb-3">
+                                <input type="text" className="form-control" id="inputLink" required />
+                                <label for="inputText">Password:</label>
+                            </div>
+                        </div>
+                         <div className="text-center mt-4"><button className="btn btn-lg px-4 fill">Log in</button></div>
+                        
+                    </form>
+                </div>
+            </div>
     )
 }
 
